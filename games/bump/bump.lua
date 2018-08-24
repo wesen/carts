8eadcf76337d5bed4ea7773ea6ea9b501aaa9198
Manuel Odendahl <wesen@ruinwesen.com>
2018-08-24 17:50:44 -0400

    Add jump

--- games/bump/bump.lua
+++ games/bump/bump.lua
@@ -1,3 +1,4 @@
+--#include constants
 --#include helpers
 --#include actors
 --#include bubbles
@@ -20,41 +21,95 @@ cls_player=subclass(typ_player,cls_actor,function(self)
 
     self.show_smoke=false
     self.prev_input=0
+    self.prev_jump=false
+    -- allows for a jump to happen for 8 frames after jump button triggered
+    self.jump_interval=0
+    -- we consider we are on the ground for 6 frames
+    self.on_ground_interval=0
+
+    self.was_on_ground=false
 end)
 
-function cls_player:update()
-    local input=btn(1) and 1 or (btn(0) and -1 or 0)
-    -- from celeste's player class
-    local maxrun=1
-    local accel=0.5
-    local decel=0.2
+function cls_player:smoke(dir)
+    add(actors,cls_smoke.init(self.pos,dir))
+end
 
-    if input!=self.prev_input and input!=0 then
-        add(actors,cls_smoke.init(self.pos,-input))
+function cls_player:update()
+    -- from celeste's player class
+    local input=btn(btn_right) and 1 
+       or (btn(btn_left) and -1 
+       or 0)
+    local jump=btn(btn_jump) and not self.prev_jump
+    self.prev_jump=btn(btn_jump)
+    if jump then
+        self.jump_interval=8
+    elseif self.jump_interval>0 then
+        self.jump_interval-=1
+    end
+
+    local maxrun=1
+    local accel=0.3
+    local decel=0.1
+
+    local on_ground=self:is_solid(v2(0,1))
+    if on_ground then
+        self.on_ground_interval=12
+    elseif self.on_ground_interval>0 then
+        self.on_ground_interval-=1
+    end
+
+    -- smoke when changing directions
+    if input!=self.prev_input and input!=0 and on_ground then
+        self:smoke(-input)
     end
     self.prev_input=input
 
+    if (not on_ground) accel=0.2
+
+    -- x movement
     if abs(self.spd.x)>maxrun then
         self.spd.x=appr(self.spd.x,sign(self.spd.x)*maxrun,decel)
     else
         self.spd.x=appr(self.spd.x,input*maxrun,accel)
     end
+    if (self.spd.x!=0) self.flip.x=self.spd.x<0
+
+    -- y movement
+    local maxfall=2
+    local gravity=0.12
+
+    -- slow down at apex
+    if (abs(self.spd.y)<=0.15) gravity*=0.5
+
+    -- wall slide
+
+    -- jump
+    if self.jump_interval>0 then
+        if self.on_ground_interval>0 then
+            self.jump_interval=0
+            self.on_ground_interval=0
+            self.spd.y=-2
+            self:smoke(v2(0,0))
+        end
+    end
+
+    if (not on_ground) self.spd.y=appr(self.spd.y,maxfall,gravity)
 
     self:move(self.spd)
 
-    if self.spd.x!=0 then
-        self.flip.x=self.spd.x<0
-    end
-
+    -- bubble instantiation
     if abs(self.spd.x)>0.9 and rnd(1)>0.93 then
         add(actors,cls_bubble.init(self.pos+v2(0,4),input))
     end
 
+    -- animation
     if input==0 then
         self.spr=1
     else
         self.spr=1+flr(frame/4)%3
     end
+
+    self.was_on_ground=on_ground
 end
 
 function cls_player:draw()
--- games/bump/constants.lua
+++ games/bump/constants.lua
@@ -3,3 +3,7 @@ typ_smoke=1
 typ_bubble=2
 
 flg_solid=0
+
+btn_right=1
+btn_left=0
+btn_jump=4
