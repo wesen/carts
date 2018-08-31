57c8549abe37014a5f8d5f8763f7cf93e144ffad
Manuel Odendahl <wesen@ruinwesen.com>
2018-08-31 15:31:50 -0400

    First level

--- games/the-moth/config.lua
+++ games/the-moth/config.lua
@@ -8,18 +8,12 @@ moth_los_limit=200
 dpal={0,1,1,2,1,13,6,4,4,9,3,13,1,13,13}
 
 levels={
- {pos=v2(0,16),
+ {pos=v2(0,0),
   dim=v2(16,16),
   -- {lamp_nr,countdown_s}
-  countdown_lights={{1,4}},
+  -- countdown_lights={{1,4}},
   -- {lamp_nr,frames_off,frames_on}
-  timer_lights={{2,128,64}}
- },
- {pos=v2(16,0),
-  dim=v2(32,16)
---  timer_lights={{3,128,16}}
- },
- {pos=v2(0,0),
-  dim=v2(16,16)}
+  -- timer_lights={{2,128,64}}
+ }
 }
 
