12b6dc9995f3b86ce52a4837402123790d527a86
Manuel Odendahl <wesen@ruinwesen.com>
2018-08-31 15:58:28 -0400

    Level 2

--- games/the-moth/config.lua
+++ games/the-moth/config.lua
@@ -9,11 +9,12 @@ dpal={0,1,1,2,1,13,6,4,4,9,3,13,1,13,13}
 
 levels={
  {pos=v2(0,0),
-  dim=v2(16,16),
+  dim=v2(16,16)},
+ {pos=v2(16,0),
+  dim=v2(16,16)}
   -- {lamp_nr,countdown_s}
   -- countdown_lights={{1,4}},
   -- {lamp_nr,frames_off,frames_on}
   -- timer_lights={{2,128,64}}
- }
 }
 
