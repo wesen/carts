537d5b86b05f6f3a91aa2098ce2d75020298fa73
Manuel Odendahl <wesen@ruinwesen.com>
2018-08-31 11:16:33 -0400

    Add vertical spikes

--- games/the-moth/config.lua
+++ games/the-moth/config.lua
@@ -10,12 +10,14 @@ dpal={0,1,1,2,1,13,6,4,4,9,3,13,1,13,13}
 levels={
  {pos=v2(0,16),
   dim=v2(16,16),
+  -- {lamp_nr,countdown_s}
   countdown_lights={{1,2}},
+  -- {lamp_nr,frames_off,frames_on}
   timer_lights={{2,128,16}}
  },
  {pos=v2(16,0),
-  dim=v2(32,16),
-  timer_lights={{3,128,16}}
+  dim=v2(32,16)
+--  timer_lights={{3,128,16}}
  },
  {pos=v2(0,0),
   dim=v2(16,16)}
--- games/the-moth/spikes.lua
+++ games/the-moth/spikes.lua
@@ -1,10 +1,17 @@
 spr_spikes=68
+spr_spikes_v=71
 
-cls_spikes=subclass(typ_spikes,cls_actor,function(self,pos)
+cls_spikes=subclass(typ_spikes,cls_actor,function(self,pos,tile)
  cls_actor._ctr(self,pos)
- self.hitbox=hitbox(v2(0,3),v2(8,5))
+ self.spr=tile
+ if tile==spr_spikes then
+  self.hitbox=hitbox(v2(0,3),v2(8,5))
+ else
+  self.hitbox=hitbox(v2(0,0),v2(8,5))
+ end
 end)
 tiles[spr_spikes]=cls_spikes
+tiles[spr_spikes_v]=cls_spikes
 
 function cls_spikes:update()
  local bbox=self:bbox()
@@ -17,8 +24,8 @@ function cls_spikes:update()
 end
 
 function cls_spikes:draw()
- spr(spr_spikes,self.pos.x,self.pos.y)
- local bbox=self:bbox()
- local bbox_col=8
- bbox:draw(bbox_col)
+ spr(self.spr,self.pos.x,self.pos.y)
+ -- local bbox=self:bbox()
+ -- local bbox_col=8
+ -- bbox:draw(bbox_col)
 end
