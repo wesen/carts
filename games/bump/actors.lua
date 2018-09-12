4668abfff62d48b78d48759ab5c19922907ecbf6
Manuel Odendahl <wesen@ruinwesen.com>
2018-09-11 22:06:35 -0400

    Return the collided actor

--- games/bump/actors.lua
+++ games/bump/actors.lua
@@ -96,7 +96,7 @@ end
 
 function cls_actor:is_actor_at(x,y)
  for actor in all(actors) do
-  if (actor.is_solid and self!=actor and do_bboxes_collide_offset(self,actor,x,y)) return true
+  if (actor.is_solid and self!=actor and do_bboxes_collide_offset(self,actor,x,y)) return true,actor
  end
 
  return false
