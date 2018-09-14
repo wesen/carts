9a4e9060bdb05bf242d43e152031d63c90880d3a
Manuel Odendahl <wesen@ruinwesen.com>
2018-09-13 21:53:23 -0400

    Add mouse cursor

--- blog/particle-systems/ps2/ps2.lua
+++ blog/particle-systems/ps2/ps2.lua
@@ -12,7 +12,11 @@ sliders={}
 
 function _init()
  -- particles_init()
- add(sliders,cls_slider.init("foobar",20,20,0.5,0,1))
+ poke(0x5f2d, 1)
+ add(sliders,cls_slider.init("slider1",20,20,0.5,0,1))
+ add(sliders,cls_slider.init("slider2",20,30,12,2,24))
+ add(sliders,cls_slider.init("slider3",20,40,50,0,100))
+ add(sliders,cls_slider.init("slider4",20,50,-80,-100,100))
 end
 
 lasttime=time()
@@ -27,10 +31,17 @@ function _update60()
  foreach(parts_1,function(p) p:update() end)
  foreach(parts_2,function(p) p:update() end)
  foreach(parts_3,function(p) p:update() end)
- foreach(sliders,function(s) s:update() end)
+ local mx=stat(32)
+ local my=stat(33)
+ local mb=band(stat(34),1)==1
+ foreach(sliders,function(s) s:update(mx,my,mb) end)
 end
 
 function _draw()
+ local mx=stat(32)
+ local my=stat(33)
+ local mb=band(stat(34),1)==1
+
  cls()
  frame+=1
 
@@ -38,5 +49,7 @@ function _draw()
  foreach(parts_1,function(p) p:draw() end)
  foreach(parts_2,function(p) p:draw() end)
  foreach(parts_3,function(p) p:draw() end)
- foreach(sliders,function(s) s:draw() end)
+ foreach(sliders,function(s) s:draw(mx,my,mb) end)
+ 
+ spr(1,mx,my)
 end
--- blog/particle-systems/ps2/slider.lua
+++ blog/particle-systems/ps2/slider.lua
@@ -1,16 +1,37 @@
 cls_slider=class(function(self,name,x,y,val,min_v,max_v)
+ self.name=name
  self.x=x
  self.y=y
  self.val=val
  self.min_v=min_v
  self.max_v=max_v
+ self.bbox={
+  aax=self.x,bbx=self.x+20,
+  aay=self.y-3,bby=self.y+3
+ }
 end)
 
-function cls_slider:draw()
+function cls_slider:draw(mx,my,mb)
  line(self.x,self.y,self.x+20,self.y,7)
  local sx=self.x+(self.val-self.min_v)/(self.max_v-self.min_v)*20
  rectfill(sx-1,self.y-3,sx+1,self.y+3,14)
+
+ if (in_bbox(self.bbox,mx,my)) print(self.name,0,110)
 end
 
-function cls_slider:update()
+function in_bbox(bbox,x,y)
+ return x>=bbox.aax and x<=bbox.bbx and y>=bbox.aay and y<=bbox.bby
+end
+
+function cls_slider:update(mx,my,mb)
+ -- bounding box for the knob
+ if mb then
+  local sx=self.x+(self.val-self.min_v)/(self.max_v-self.min_v)*20
+  local bbox={
+   aax=sx-1,
+   bbx=sx+1,
+   aay=self.y-3,
+   bby=self.y+3
+  }
+ end
 end
