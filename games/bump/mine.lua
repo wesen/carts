d8b2a7ebd5ec33474507fb44d2061a13ec23a1a2
Manuel Odendahl <wesen@ruinwesen.com>
2018-09-15 16:34:34 -0400

    Add radius to power blast function

--- games/bump/mine.lua
+++ games/bump/mine.lua
@@ -5,20 +5,20 @@ cls_mine=subclass(cls_interactable,function(self,pos)
  self.spr=spr_mine
 end)
 
-function make_blast(x,y)
+function make_blast(x,y,radius)
  add_cr(function ()
   for i=0,20 do
-   local r=outexpo(i,50,-50,20)
+   local r=outexpo(i,radius,-radius,20)
    circfill(x+4,y+6,r,7)
    yield()
   end
  end, draw_crs)
  for p in all(players) do
-  if p.power_up!=spr_power_up_invincibility then
+  if p.power_up!=spr_pwrup_invincibility then
    local dx=p.x-x
    local dy=p.y-y
    local d=sqrt(dx*dx+dy*dy)
-   if d<50 then
+   if d<radius then
     p:kill()
     make_gore_explosion(v2(p.x,p.y))
    end
@@ -27,7 +27,7 @@ function make_blast(x,y)
 end
 
 function cls_mine:on_player_collision(player)
- make_blast(self.x,self.y)
+ make_blast(self.x,self.y,30)
  del(interactables,self)
 end
 tiles[spr_mine]=cls_mine
@@ -37,7 +37,7 @@ cls_suicide_bomb=subclass(cls_pwrup,function(self,pos)
 end)
 
 function cls_suicide_bomb:on_powerup_stop(player)
- if (player.power_up_countdown<=0) make_blast(player.x,player.y)
+ if (player.power_up_countdown<=0) make_blast(player.x,player.y,30)
 end
 
 spr_suicide_bomb=45
