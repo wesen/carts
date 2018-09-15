148f2c76d884729fb2cf615a78731da3424def02
Manuel Odendahl <wesen@ruinwesen.com>
2018-09-15 16:34:23 -0400

    Better power ups dropper behaviour

--- games/bump/power-ups.lua
+++ games/bump/power-ups.lua
@@ -1,31 +1,41 @@
-drop_min_time=60*5
-drop_max_time=60*30
+drop_min_time=60*4
+drop_max_time=60*10
 
+max_count=2
+
+power_up_droppers={}
 
 cls_pwrup_dropper=subclass(cls_actor,function(self,pos)
  cls_actor._ctr(self,pos)
  self.is_solid=false
- -- Set spawn time between min time and max time
- self.interval=flr(drop_min_time+(rnd(1)*(drop_max_time-drop_min_time)))
+ -- set spawn time between min time and max time
  self.time=0
  self.item=nil
+ self.interval=1
+ add(power_up_droppers,self)
 end)
+
+local pwrup_counts=0
 
 function cls_pwrup_dropper:update()
  if self.item==nil then
-
-  -- Increment time. Spawn when time's up
+  -- increment time. spawn when time's up
   self.time=(self.time%(self.interval))+1
-  if self.time==self.interval then
-   local cls_idx=flr(rnd(#power_ups))
-   local spr_idx=cls_idx+spr_idx_start
-   self.item=power_ups[cls_idx+1].init(v2(self.x,self.y))
-   self.item.tile=spr_idx
+  if self.time>=self.interval then
+   printh("dropper time interval "..tostr(self.time).." "..tostr(self.interval))
+   if pwrup_counts<max_count then
+    local spr_idx=power_up_tiles[flr(rnd(#power_up_tiles))+1]
+    printh("load spr "..tostr(spr_idx).." "..tostr(pwrup_counts))
+    self.item=tiles[spr_idx].init(v2(self.x,self.y))
+    self.item.tile=spr_idx
+    pwrup_counts+=1
+   end
+   self.interval=flr(drop_min_time+(rnd(1)*(drop_max_time-drop_min_time)))
   end
 
  else
 
-  -- Check that item has been used before allowing another drop
+  -- check that item has been used before allowing another drop
   local exists=false
   for interactable in all(interactables) do
    if interactable==self.item then
@@ -34,12 +44,21 @@ function cls_pwrup_dropper:update()
   end
 
   if not exists then
+   printh("decreasing")
+   pwrup_counts-=1
    self.item=nil
    self.interval=flr(drop_min_time+(rnd(1)*(drop_max_time-drop_min_time)))
+   if pwrup_counts<max_count then
+    for dropper in all(power_up_droppers) do
+     dropper.time=0
+    end
+   end
   end
-
  end
 end
+
+spr_pwrup_dropper=25
+tiles[spr_pwrup_dropper]=cls_pwrup_dropper
 
 
 cls_pwrup=subclass(cls_interactable,function(self,pos)
@@ -93,49 +112,58 @@ end
 powerup_colors={}
 powerup_countdowns={}
 
-spr_power_up_doppelgaenger=39
---tiles[spr_power_up_doppelgaenger]=cls_pwrup_doppelgaenger
-powerup_colors[spr_power_up_doppelgaenger]=8
 
-spr_power_up_invincibility=40
---tiles[spr_power_up_invincibility]=cls_pwrup
-powerup_colors[spr_power_up_invincibility]=9
-powerup_countdowns[spr_power_up_invincibility]=10
+spr_pwrup_doppelgaenger=39
+--tiles[spr_pwrup_doppelgaenger]=cls_pwrup_doppelgaenger
+powerup_colors[spr_pwrup_doppelgaenger]=8
 
-spr_power_up_superspeed=41
---tiles[spr_power_up_superspeed]=cls_pwrup
-powerup_colors[spr_power_up_superspeed]=6
-powerup_countdowns[spr_power_up_superspeed]=10
+spr_pwrup_invincibility=40
+--tiles[spr_pwrup_invincibility]=cls_pwrup
+powerup_colors[spr_pwrup_invincibility]=9
+powerup_countdowns[spr_pwrup_invincibility]=10
 
-spr_power_up_superjump=42
---tiles[spr_power_up_superjump]=cls_pwrup
-powerup_colors[spr_power_up_superjump]=12
-powerup_countdowns[spr_power_up_superjump]=15
+spr_pwrup_superspeed=41
+--tiles[spr_pwrup_superspeed]=cls_pwrup
+powerup_colors[spr_pwrup_superspeed]=6
+powerup_countdowns[spr_pwrup_superspeed]=10
 
-spr_power_up_gravitytweak=43
---tiles[spr_power_up_gravitytweak]=cls_pwrup
-powerup_colors[spr_power_up_gravitytweak]=9
-powerup_countdowns[spr_power_up_gravitytweak]=30
+spr_pwrup_superjump=42
+--tiles[spr_pwrup_superjump]=cls_pwrup
+powerup_colors[spr_pwrup_superjump]=12
+powerup_countdowns[spr_pwrup_superjump]=15
 
-spr_power_up_invisibility=44
---tiles[spr_power_up_invisibility]=cls_pwrup
-powerup_countdowns[spr_power_up_invisibility]=30
+spr_pwrup_gravitytweak=43
+--tiles[spr_pwrup_gravitytweak]=cls_pwrup
+powerup_colors[spr_pwrup_gravitytweak]=9
+powerup_countdowns[spr_pwrup_gravitytweak]=30
 
-spr_power_up_shrink=46
---tiles[spr_power_up_shrink]=cls_pwrup
-powerup_countdowns[spr_power_up_shrink]=30
+spr_pwrup_invisibility=44
+--tiles[spr_pwrup_invisibility]=cls_pwrup
+powerup_countdowns[spr_pwrup_invisibility]=5
 
+spr_pwrup_shrink=46
+--tiles[spr_pwrup_shrink]=cls_pwrup
+powerup_countdowns[spr_pwrup_shrink]=10
 
--- Start offset for the item sprite values
+-- start offset for the item sprite values
 spr_idx_start=39
--- Associate sprite value with class
-power_ups={ 
- cls_pwrup_doppelgaenger,
- cls_pwrup,
- cls_pwrup,
- cls_pwrup,
- cls_pwrup,
- cls_pwrup,
- cls_pwrup
+-- associate sprite value with class
+tiles[spr_pwrup_doppelgaenger]=cls_pwrup_doppelgaenger
+tiles[spr_pwrup_invisibility]=cls_pwrup
+tiles[spr_pwrup_shrink]=cls_pwrup
+tiles[spr_pwrup_invincibility]=cls_pwrup
+tiles[spr_pwrup_superjump]=cls_pwrup
+tiles[spr_pwrup_superspeed]=cls_pwrup
+tiles[spr_pwrup_gravitytweak]=cls_pwrup
+
+power_up_tiles={
+ spr_pwrup_doppelgaenger,
+ -- spr_pwrup_invincibility,
+ -- spr_pwrup_superjump,
+ spr_pwrup_invisibility,
+ spr_pwrup_invisibility,
+ spr_pwrup_invisibility,
+ -- spr_pwrup_superspeed,
+ -- spr_pwrup_gravitytweak,
+ spr_pwrup_shrink
 }
-tiles[spr_power_up_doppelgaenger]=cls_pwrup_dropper
