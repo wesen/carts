46da9ac5f0dc3463117efbb3d7d20dd213d182f2
Manuel Odendahl <wesen@ruinwesen.com>
2018-09-19 10:47:32 -0400

    Refactor start transition code in preparation for end transition

--- games/bump/main.lua
+++ games/bump/main.lua
@@ -7,43 +7,52 @@ mode_transition=3
 
 mode=mode_title
 
+function make_transition(mid_cb,end_cb)
+ add_cr(function()
+  local col=7
+  local duration=15
+  for i=0,duration do
+   palt(col,false)
+   local radius=inexpo(i,0,128,duration)
+   circfill(64,64,radius,col)
+   palt()
+   yield()
+  end
+  mid_cb()
+  for i=0,duration do
+   palt(col,false)
+   local radius=128-inexpo(i,0,128,duration)
+   circfill(64,64,radius,col)
+   palt()
+   yield()
+  end
+ end_cb()
+ end, draw_crs)
+end
+
 function start_game()
- local inputs={}
- for p in all(players) do
-  add(inputs,p.input_port)
- end
+ -- this is a pretty inelegant way of resetting global state
  interactables={}
  actors={}
  players={}
+ particles={}
+ fireflies_init(v2(16,16))
+ crs={}
  pwrup_counts=0
  for i=1,4 do
   scores[i]=0
  end
-  add_cr(function()
-   local duration=15
-   for i=0,duration do
-    palt(0,false)
-    local radius=inexpo(i,0,128,duration)
-    printh("radius "..tostr(radius))
-    circfill(64,64,radius,8)
-    palt()
-    yield()
-   end
-   mode=mode_transition
-   room=cls_room.init(v2(0,16),v2(16,16))
-   for i=0,duration do
-    palt(0,false)
-    local radius=128-inexpo(i,0,128,duration)
-    printh("radius "..tostr(radius))
-    circfill(64,64,radius,8)
-    palt()
-    yield()
-   end
-   mode=mode_game
-   for input in all(inputs) do
+ make_transition(function()
+  mode=mode_transition
+  room=cls_room.init(v2(0,16),v2(16,16))
+ end, function()
+  mode=mode_game
+  for input,v in pairs(connected_players) do
+   if v==true then
     room:spawn_player(input)
    end
-  end, draw_crs)
+  end
+ end)
 end
 
 function end_game()
@@ -70,6 +79,7 @@ function _init()
  room:spawn_player(p3_input)
  fireflies_init(v2(16,16))
  music(0)
+ -- start_game()
 end
 
 function update_a(a) a:update() end
