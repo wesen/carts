77db3f37e5d400f823154cf5aa62730fa78315f6
Manuel Odendahl <wesen@ruinwesen.com>
2018-09-19 10:28:11 -0400

    Properly clean game when switching to game start

--- games/bump/main.lua
+++ games/bump/main.lua
@@ -3,20 +3,47 @@ winning_player=nil
 mode_title=0
 mode_game=1
 mode_end=2
+mode_transition=3
 
 mode=mode_title
 
 function start_game()
- room=cls_room.init(v2(0,16),v2(16,16))
+ local inputs={}
  for p in all(players) do
-  del(players,p)
-  del(actors,p)
-  room:spawn_player(p.input_port)
+  add(inputs,p.input_port)
  end
+ interactables={}
+ actors={}
+ players={}
+ pwrup_counts=0
  for i=1,4 do
   scores[i]=0
  end
- mode=mode_game
+  add_cr(function()
+   local duration=15
+   for i=0,duration do
+    palt(0,false)
+    local radius=inexpo(i,0,128,duration)
+    printh("radius "..tostr(radius))
+    circfill(64,64,radius,8)
+    palt()
+    yield()
+   end
+   mode=mode_transition
+   room=cls_room.init(v2(0,16),v2(16,16))
+   for i=0,duration do
+    palt(0,false)
+    local radius=128-inexpo(i,0,128,duration)
+    printh("radius "..tostr(radius))
+    circfill(64,64,radius,8)
+    palt()
+    yield()
+   end
+   mode=mode_game
+   for input in all(inputs) do
+    room:spawn_player(input)
+   end
+  end, draw_crs)
 end
 
 function end_game()
@@ -59,30 +86,34 @@ function _draw()
   end
  end
 
- if mode!=mode_end then
-  camera(camera_shake.x,camera_shake.y)
-  room:draw()
-  foreach(interactables, draw_a)
-  foreach(environments, draw_a)
-  foreach(static_objects, draw_a)
-  draw_actors()
-
-  tick_crs(draw_crs)
-  fireflies_draw()
-  foreach(particles, draw_a)
-
-  if mode==mode_game then
-   local entry_length=30
-   for i=0,#scores-1,1 do
-    print(
-    "p"..tostr(i+1)..": "..tostr(scores[i+1]),
-    i*entry_length,1,7
-    )
-   end
-  end
+ if mode==mode_transition then
+   tick_crs(draw_crs)
  else
-  rectfill(0,0,128,128,8)
-  bstr("player "..tostr(winning_player).." won!",38,64,7,1)
+  if mode!=mode_end then
+   camera(camera_shake.x,camera_shake.y)
+   room:draw()
+   foreach(interactables, draw_a)
+   foreach(environments, draw_a)
+   foreach(static_objects, draw_a)
+   draw_actors()
+
+   fireflies_draw()
+   foreach(particles, draw_a)
+   tick_crs(draw_crs)
+
+   if mode==mode_game then
+    local entry_length=30
+    for i=0,#scores-1,1 do
+     print(
+     "p"..tostr(i+1)..": "..tostr(scores[i+1]),
+     i*entry_length,1,7
+     )
+    end
+   end
+  else
+   rectfill(0,0,128,128,8)
+   bstr("player "..tostr(winning_player).." won!",38,64,7,1)
+  end
  end
 end
 
